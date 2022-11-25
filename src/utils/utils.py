8aeca8ac23439fa8a185a6bd0d1e159c926e264b
nguyen anh quyet <quyetna.img@gmail.com>
2022-11-25 18:05:21 +0700

    orc

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,5 +2,5 @@ import cv2 as cv
 import time
 import numpy as np
 
+# https://github.com/immanuvelprathap/OpenCV-Tesseract-EAST-Text-Detector/blob/8d02e1dd980631cceab95d44da8738dac21f1e4e/utils.py
 def forward_passer(net, image, layers, timing = True):
-    pass
